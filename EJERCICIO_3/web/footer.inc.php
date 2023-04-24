2b41b4150fe806eb777dd489cd6e96bb2f5c81c0
Jael-Lany-Valeriano-Baltazar <130415629+Jael-Lany-Valeriano-Baltazar@users.noreply.github.com>
2023-04-24 13:31:29 -0400

    Add files via upload

---
+++ EJERCICIO_3/web/footer.inc.php
@@ -0,0 +1,60 @@
+<?php 
+
+session_start();
+$USER = $_SESSION["USER"];
+
+?>
+    <!-- Footer -->
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-container about">
+                        <h4>UMSA</h4>
+                        <h5  style="color: silver;">Facultad de Ciencias Puras y Naturales</h5>
+                        <h6  style="color: silver;">Carrera de Informática</h6>
+                        <p class="white"  style="color: silver;">INF - 324 | Programación Multimedia</p>
+                        <p class="white"  style="color: silver;">EXAMEN PRÁCTICO - PRIMER PARCIAL</p>
+                        <p class="p-small">Copyright © 2023 </p>
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+                <div class="col-md-2"></div>
+                <div class="col-md-4">
+                    <div class="text-conainer">
+                        <h4>Enlaces</h4>
+                        <ul class="list-unstyled li-space-lg">
+                            <li>
+                                <a class="white" href="https://github.com/Jael-Lany-Valeriano-Baltazar/INF-317.git"><img src="../logos/github.png" alt=""></a>
+                            </li>
+                            <li>
+                               <p style="color: silver;">Estudiante: Jael L. Valeriano Baltazar</p>
+                            </li>
+                            <li>
+                                <a class="white" href="https://cvinformatica.umsa.bo/enrol/index.php?id=49"><img src="../logos/moodle-logo-white.png" width="100px" alt=""></a>
+                            </li>
+                            <li class="media">
+                                <p style="color: silver;">Docente: Moises Silva</p>
+                            </li>
+                        </ul>
+                    </div> <!-- end of text-container -->
+                </div> <!-- end of col -->
+            </div> <!-- end of row -->
+        </div> <!-- end of container -->
+    </div> <!-- end of footer -->  
+    <!-- end of footer -->
+
+
+    	
+    <!-- Scripts -->
+    <script src="js/jquery.min.js"></script> <!-- jQuery for Bootstrap's JavaScript plugins -->
+    <script src="js/popper.min.js"></script> <!-- Popper tooltip library for Bootstrap -->
+    <script src="js/bootstrap.min.js"></script> <!-- Bootstrap framework -->
+    <script src="js/jquery.easing.min.js"></script> <!-- jQuery Easing for smooth scrolling between anchors -->
+    <script src="js/swiper.min.js"></script> <!-- Swiper for image and text sliders -->
+    <script src="js/jquery.magnific-popup.js"></script> <!-- Magnific Popup for lightboxes -->
+    <script src="js/morphext.min.js"></script> <!-- Morphtext rotating text in the header -->
+    <script src="js/isotope.pkgd.min.js"></script> <!-- Isotope for filter -->
+    <script src="js/validator.min.js"></script> <!-- Validator.js - Bootstrap plugin that validates forms -->
+    <script src="js/scripts.js"></script> <!-- Custom scripts -->
+</body>
+</html>
---
+++ EJERCICIO_3/web/inicio.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+$USER = $_SESSION["USER"];
+include "nav.inc.php";
+include "header.inc.php";
+
+include "menu.inc.php";
+include "footer.inc.php";
+?>
+
+
+
+    
+  
+
