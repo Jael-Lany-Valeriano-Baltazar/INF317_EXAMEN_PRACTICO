4332705df1a09b484aa6a0e080ab7a9a1895730f
Jael-Lany-Valeriano-Baltazar <130415629+Jael-Lany-Valeriano-Baltazar@users.noreply.github.com>
2023-04-24 13:19:17 -0400

    Add files via upload

---
+++ EJERCICIO_1/web/inicio.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$USER = $_SESSION["username"];
+ 
+include "nav.inc.php";
+include "header.inc.php";
+
+include "menu.inc.php";
+include "footer.inc.php";
+?>
+
+
+
+    
+  
+
